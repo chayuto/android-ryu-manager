84eabafaffcfb31b29325484fd276bbb4314a9b1
chayut orapinpatipat <chayut_o@hotmail.com>
2016-03-27 17:39:06 +1100

    Connnection task with ASYNC

--- app/src/main/java/me/chayut/ryumanager/MainActivity.java
+++ app/src/main/java/me/chayut/ryumanager/MainActivity.java
@@ -1,10 +1,19 @@
 package me.chayut.ryumanager;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnConnect;
     CheckBox cbConnect;
+    EditText etServerAddr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +29,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnConnect = (Button) findViewById(R.id.btnConnect);
+        etServerAddr = (EditText ) findViewById(R.id.editText);
         cbConnect = (CheckBox) findViewById(R.id.checkBoxRemember);
 
     }
 
-    private void onButtonPressed(View view){
+    public void onButtonPressed(View view){
+
+        String serverAddress = etServerAddr.getText().toString();
+        Log.d(TAG,"Address: " + serverAddress);
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new CheckConnectivityTask().execute(serverAddress);
+        } else {
+            Log.d(TAG,"No network connection available.");
+        }
+
+
 
     }
+
+    public boolean isConnectedToServer(String url, int timeout) {
+        try{
+            URL myUrl = new URL(url);
+            URLConnection connection = myUrl.openConnection();
+            connection.setConnectTimeout(timeout);
+            connection.connect();
+            return true;
+        } catch (Exception e) {
+            // Handle your exceptions
+            return false;
+        }
+    }
+
+    private class CheckConnectivityTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... urls) {
+
+            return isConnectedToServer(urls[0],30);
+
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(Boolean result) {
+
+            //TODO:reponse to result
+        }
+    }
+
 }
--- app/src/main/java/me/chayut/ryumanager/SwitchesActivity.java
+++ app/src/main/java/me/chayut/ryumanager/SwitchesActivity.java
@@ -2,12 +2,69 @@ package me.chayut.ryumanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SwitchesActivity extends AppCompatActivity {
+
+    private final static String TAG = "SwitchesActivity";
+
+    // Instantiate the RequestQueue.
+    RequestQueue queue ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_switches);
+
+
+        //query queue
+        queue = Volley.newRequestQueue(this);
+
+    }
+
+    public void getResultsList (){
+
+        String username ="chayut_orapinpatipat";
+        String codekey = "6be4a4bbf9a57b39ffd296f29e899309";
+
+
+        String url = String.format("https://secure.worldcommunitygrid.org/api/members/%s/" +
+                "results?code=%s&limit=20&SortBy=ReceivedTime&ServerState=5", username, codekey);
+        //&ValidateState=0
+
+
+
+        Log.d(TAG, url);
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                    }
+                });
+
+
+
+        // Add the request to the RequestQueue.
+        queue.add(jsObjRequest);
     }
 }
